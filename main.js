8f125fd5a95dcab51c270927e441eee5b5660765
Cesar Rojas <cesar.fieoner@gmail.com>
2018-08-18 21:19:17 +0200

    streams yay!

--- main.js
+++ main.js
@@ -3,12 +3,18 @@ const request = require('request-promise')
 const base64Img = require('base64-img')
 const fs = require('fs')
 const Promise = require('bluebird')
+const bigInt = require('big-integer')
 
 const EMULATOR_URL = process.env.EMULATOR_URL || 'http://localhost:8123'
+
+const DELAY = 30e3
 
 let LAST_MENTION_ID = 706829683887837200
 
 const VALID_MOVES = [ 'a', 'b', 'up', 'down', 'left', 'right', 'start', 'select' ]
+
+let moves = {}
+const resetPoll = (moves) => {VALID_MOVES.forEach(item => moves[item] = 0)}
 
 const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -49,51 +55,88 @@ const tweet = async (data = {}) => {
 }
 
 const run = async () => {
-  if (false) {
+  resetPoll(moves)
+  if (true) {
     // TODO
   }
   else {
     // new game
     const startData = JSON.parse(await runEmulator('start'))
-    await tweet({
+    await Promise.all([tweet({
       text: 'Aaaaand the game begins... Pick the first button to press (a, b, up, down, left, right, select, start)',
       image: startData.result.screenshot
-    }).catch(console.error)
+    }).catch(console.error),
+    Promise.delay(DELAY)])
   }
-  return turn()
+  return turn(moves)
 }
 
-const turn = async () => {
-  const nextMove = await getNextMove()
-  if (!nextMove) return Promise.delay(30e3).then(turn)
-  console.log(nextMove)
+const stream = client.stream('statuses/filter', {track: '@WePlayBot'})
+stream.on('data', event => {
+  if (event.in_reply_to_user_id == "4614087921"){
+    console.log("event: "+event)
+    console.log("text: "+event.text)
+    const move = event.text.toLowerCase().replace('@weplaybot', '').trim()
+    console.log("moves2times")
+    console.log(moves)
+    if (VALID_MOVES.indexOf(move) > -1)
+      moves[move] += 1
+    console.log(moves)
+  }
+})
+stream.on('error', error => {
+  console.error(error)
+})
+
+const winnerMove = async (moves) => {
+  var result = 0
+  var winner = 'nothing'
+  console.log("win")
+  console.log(moves)
+  Object.keys(moves).forEach(move => {
+    if (moves[move] > result){
+      result = moves[move]
+      winner = move
+    }
+  })
+  resetPoll(moves)
+  return winner
+}
+
+const turn = async (moves) => {
+  console.log(moves)
+  const nextMove = await winnerMove(moves)
+  if (nextMove == 'nothing') return Promise.delay(DELAY).then(() => turn(moves))
   const aData = JSON.parse(await runEmulator('execute', { key: nextMove }))
   await Promise.all([
     tweet({
       text: 'This is the result of the last move. Pick the next button to press (a, b, up, down, left, right, select, start)',
       image: aData.result.screenshot
     }).catch(console.error),
-    Promise.delay(30e3)
+    Promise.delay(DELAY)
   ])
-  return turn()
+  turn(moves)
 }
 
-const getNextMove = async () => {
-  console.log('before')
-  const mentions = await client.get('statuses/mentions_timeline', {
-    since_id: LAST_MENTION_ID
-  })
-  const lastMention = mentions.find(x => {
-    const text = x.text.replace('@WePlayBot', '').trim()
-    return VALID_MOVES.indexOf(text) > -1
-  })
-  console.log(mentions)
-  console.log('after')
-  console.log(lastMention)
-  if (lastMention) LAST_MENTION_ID = lastMention.id
-  else return false
 
-  return lastMention.text.replace('@WePlayBot', '').trim()
-}
+
+// const getNextMove = async () => {
+//   console.log('before')
+//   console.log('last mention id', LAST_MENTION_ID)
+//   const mentions = await client.get('statuses/mentions_timeline', {
+//     since_id: LAST_MENTION_ID
+//   })
+//   const lastMention = mentions.find(x => {
+//     const text = x.text.toLowerCase().replace('@weplaybot', '').trim()
+//     return VALID_MOVES.indexOf(text) > -1
+//   })
+//   // console.log(mentions)
+//   console.log('after')
+//   // console.log(lastMention)
+//   if (lastMention) LAST_MENTION_ID = bigInt(lastMention.id_str).add(1).toString()
+//   else return false
+//
+//   return lastMention.text.toLowerCase().replace('@weplaybot', '').trim()
+// }
 
 run().catch(console.error)
