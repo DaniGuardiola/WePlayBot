45075b5322d405668b315a45206f5e279194c438
Dani <contact@daniguardiola.me>
2018-08-24 18:22:25 +0200

    fixed linting errors

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const request = require('request-promise')
 const base64Img = require('base64-img')
 const fs = require('fs')
 const Promise = require('bluebird')
-const bigInt = require('big-integer')
 const db = require('node-persist')
 
 const EMULATOR_URL = process.env.EMULATOR_URL || 'http://localhost:8123'
@@ -14,7 +13,7 @@ const VALID_MOVES = [ 'a', 'b', 'up', 'down', 'left', 'right', 'start', 'select'
 
 let moves = {}
 
-const resetPoll = (moves) => {VALID_MOVES.forEach(item => moves[item] = 0)}
+const resetPoll = moves => VALID_MOVES.forEach(item => (moves[item] = 0))
 
 const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -46,7 +45,7 @@ const tweet = async (data = {}) => {
   const media = await client.post('media/upload', { media: image })
 
   const status = {
-    status: "@WePlayBot "+data.text,
+    status: '@WePlayBot ' + data.text,
     media_ids: media.media_id_string,
     in_reply_to_status_id: data.in_reply_to_status_id
   }
@@ -56,28 +55,27 @@ const tweet = async (data = {}) => {
 const run = async () => {
   await db.init({
     dir: '.persistence',
-  	logging: false,
+    logging: false
   })
   resetPoll(moves)
-  const game_started = await db.getItem('game_started')
-  if (game_started == 'yes') {
+  const gameStarted = await db.getItem('gameStarted')
+  if (gameStarted === 'yes') {
     // TODO
-  }
-  else {
+  } else {
     // new game
     const startData = JSON.parse(await runEmulator('start'))
     const lastTweet = await db.getItem('lastTweet')
     await Promise.all([
       tweet({
         text: 'Aaaaand the game begins... Pick the first button to press (a, b, up, down, left, right, select, start)',
-        image: aData.result.screenshot,
+        image: startData.result.screenshot,
         in_reply_to_status_id: lastTweet
       }).then(status => {
         db.setItem('lastTweet', status.id_str)
       }).catch(console.error),
       Promise.delay(DELAY)
     ])
-    db.setItem('game_started', 'yes')
+    db.setItem('gameStarted', 'yes')
   }
   return turn(moves)
 }
@@ -85,14 +83,14 @@ const run = async () => {
 const stream = client.stream('statuses/filter', {track: '@WePlayBot'})
 stream.on('data', async event => {
   const lastTweet = await db.getItem('lastTweet')
-  if (event.in_reply_to_user_id == "4614087921" && event.in_reply_to_status_id == lastTweet){
-    console.log("new tweet text: "+event.text)
-    const clean_tweet = event.text.toLowerCase().replace('@weplaybot', '').trim().split(' ')
-    const move = VALID_MOVES.find(valid_move => {
-      return clean_tweet.includes(valid_move)
+  if (event.in_reply_to_user_id === '4614087921' && event.in_reply_to_status_id === lastTweet) {
+    console.log('new tweet text: ' + event.text)
+    const cleanTweet = event.text.toLowerCase().replace('@weplaybot', '').trim().split(' ')
+    const move = VALID_MOVES.find(validMove => {
+      return cleanTweet.includes(validMove)
     })
     if (move) {
-      console.log("move found: "+move)
+      console.log('move found: ' + move)
       moves[move] += 1
     }
   }
@@ -105,7 +103,7 @@ const getNextMove = async (moves) => {
   let result = 0
   let winner = 'nothing'
   Object.keys(moves).forEach(move => {
-    if (moves[move] > result){
+    if (moves[move] > result) {
       result = moves[move]
       winner = move
     }
@@ -115,11 +113,11 @@ const getNextMove = async (moves) => {
 }
 
 const turn = async (moves) => {
-  console.log("moves for this turn:")
+  console.log('moves for this turn:')
   console.log(moves)
 
   const nextMove = await getNextMove(moves)
-  if (nextMove == 'nothing') return Promise.delay(DELAY).then(() => turn(moves))
+  if (nextMove === 'nothing') return Promise.delay(DELAY).then(() => turn(moves))
 
   const aData = JSON.parse(await runEmulator('execute', { key: nextMove }))
   const lastTweet = await db.getItem('lastTweet')
